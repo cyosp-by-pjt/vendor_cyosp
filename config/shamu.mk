62704404051db4b1cfc20f0fd91310adfafa638a
pjt <polyak.tamas@gmail.com>
2016-04-23 14:41:24 +0200

    Update to April 2016 security update MOB30D

---
+++ config/shamu.mk
@@ -0,0 +1,5 @@
+#set fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	PRODUCT_NAME=shamu \
+	BUILD_FINGERPRINT="google/shamu/shamu:6.0.1/MOB30D/2704746:user/release-keys" \
+    PRIVATE_BUILD_DESC="shamu-user 6.0.1 MOB30D 2704746 release-keys"
